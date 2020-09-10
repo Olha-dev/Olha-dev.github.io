a45ae65ef2ba7a4310e0e3fd1753ca62d33862d7
Olha-dev <71031581+Olha-dev@users.noreply.github.com>
2020-09-10 21:53:52 +0300

    Add files via upload

---
+++ indexphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+  <title><em>Olha Sqvchenko 1395ae</em></title>
+  <?php
+  $stuff=array("1","2","3","4","5","6");
+foreach($stuff as $k=>$v){
+if(count($stuff)<$k){
+echo ("Your guess is too high\n"};}
+if(count($stuff)>$k){
+echo("Your guess is too low\n");}
+if (array_key_exists($key.$stuff)&&array_key_exists($value.$stuff)){
+echo("key and value are set\n");}
+else{ echo("Missing guess parameters\n");}
+  if($k===$v){
+  echo("Congratulations-You are right\n");}
+  else{("Your guess is not a number");}
+  ?>
+</body>
+</html>
